6fa88d7626d9ed3fab7b1f58d7572228a2d1a00c
Jack Gang <jack.gang@xrtrading.com>
2017-10-06 13:10:42 -0500

    remove unimpactful variables

--- Safe Driver/SafeDriver.py
+++ Safe Driver/SafeDriver.py
@@ -17,6 +17,8 @@ from pandas import DataFrame
 # numpy, matplotlib, seaborn
 import numpy as np
 import matplotlib.pyplot as plt
+import plotly.offline as py
+import plotly.graph_objs as go
 import seaborn as sns
 sns.set_style('whitegrid')
 #%matplotlib inline
@@ -151,6 +153,93 @@ test_df = test_df_copy.copy()
 
 #################################################################
 
+# look at binary variables
+
+bin_col = [col for col in driver_df.columns if '_bin' in col]
+zero_list = []
+one_list = []
+for col in bin_col:
+    zero_list.append((driver_df[col]==0).sum())
+    one_list.append((driver_df[col]==1).sum())
+#    print(col, "% of 1s:", (driver_df[col]==1).sum()/((driver_df[col]==0).sum() + (driver_df[col]==1).sum()))
+    
+trace1 = go.Bar(
+    x=bin_col,
+    y=zero_list ,
+    name='Zero count'
+)
+trace2 = go.Bar(
+    x=bin_col,
+    y=one_list,
+    name='One count'
+)
+
+data = [trace1, trace2]
+layout = go.Layout(
+    barmode='stack',
+    title='Count of 1 and 0 in binary variables'
+)
+
+fig = go.Figure(data=data, layout=layout)
+py.iplot(fig, filename='stacked-bar')
+
+# remove variables that are < 1% binary
+driver_df = driver_df.drop(['ps_ind_10_bin','ps_ind_11_bin','ps_ind_12_bin','ps_ind_13_bin'], axis=1)
+test_df = test_df.drop(['ps_ind_10_bin','ps_ind_11_bin','ps_ind_12_bin','ps_ind_13_bin'], axis=1)
+
+#################################################################
+
+# look for relevant categorical variables
+
+threshold = 0.005
+
+for varName in [col for col in driver_df.columns if ('_bin' in col) or ('_cat' in col)]:
+#    varName = 'ps_ind_18_bin'
+    #fig, (axis1,axis2,axis3) = plt.subplots(1,3,figsize=(15,5))
+    
+    #sns.countplot(x=varName, data=driver_df, ax=axis1)
+    #sns.countplot(x='target', hue=varName, data=driver_df, order=[1,0], ax=axis2)
+    
+    # group by var, and get the mean for targeted people for each value in var
+    var_perc = driver_df[[varName, "target"]].groupby([varName],as_index=False).mean()
+    if (max(var_perc['target']) - min(var_perc['target'])) < threshold:
+        print(varName, max(var_perc['target']) - min(var_perc['target']))
+        driver_df = driver_df.drop([varName], axis=1)
+        test_df = test_df.drop([varName], axis=1)        
+    #sns.barplot(x=varName, y='target', data=var_perc,order=[0,1],ax=axis3)
+    
+#################################################################
+
+# look for relevant continuous variables
+
+threshold = 0.05
+
+for varName in [col for col in driver_df.columns if ('_bin' not in col) and ('_cat' not in col)]:
+    if varName in ['id','target']:
+        continue
+
+    # get fare for target and not target people 
+    var_not_target = driver_df[varName][driver_df["target"] == 0]
+    var_target     = driver_df[varName][driver_df["target"] == 1]
+    
+    # get average and std for fare of survived/not survived passengers
+    avg_var = DataFrame([var_not_target.mean(), var_target.mean()])
+    std_var = DataFrame([var_not_target.std(), var_target.std()])
+    
+    var_range = 2*driver_df[varName].std()
+    diff_ratio = (max(avg_var[0]) - min(avg_var[0])) / var_range
+    if diff_ratio < threshold:
+        print(varName, diff_ratio)
+        driver_df = driver_df.drop([varName], axis=1)
+        test_df = test_df.drop([varName], axis=1)      
+    
+#    # plot
+#    driver_df[varName].plot(kind='hist', figsize=(15,3),bins=100, xlim=(min(driver_df[varName]),max(driver_df[varName])))
+#    avg_var.index.names = std_var.index.names = ["target"]
+#    avg_var.plot(yerr=std_var,kind='bar',legend=False)
+
+#################################################################
+
 # define training and testing sets
 
 X_train = driver_df.drop("target",axis=1)
@@ -215,4 +304,4 @@ coeff_df
 
 submission = pd.DataFrame({"id": test_df["id"], "target": Y_pred})
     
-submission.to_csv("result2.csv", header = ['id','target'], index = False, quoting = csv.QUOTE_NONE, quotechar = '')
+submission.to_csv("result4.csv", header = ['id','target'], index = False, quoting = csv.QUOTE_NONE, quotechar = '')
